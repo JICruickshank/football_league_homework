549d63bf7ecdc91f03c96d37720c42eccad84d05
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-03-18 15:52:54 +0000

    adds competition class

---
+++ src/main/java/models/Competition.java
@@ -0,0 +1,45 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Competition {
+
+    private String name;
+    private List<Team> teams;
+    private int id;
+
+    public Competition(String name, List<Team> teams) {
+        this.name = name;
+        this.teams = new ArrayList<Team>();
+    }
+
+    public Competition() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
